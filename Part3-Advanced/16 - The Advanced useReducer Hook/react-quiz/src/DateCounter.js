a7b113e558e4aef279cf54c72ac4ac85513544d0
RyzenMu <muruga13in2022@gmail.com>
2025-01-11 09:13:53 +0530

    chapter 188 started

--- Part3-Advanced/16 - The Advanced useReducer Hook/react-quiz/src/DateCounter.js
+++ Part3-Advanced/16 - The Advanced useReducer Hook/react-quiz/src/DateCounter.js
@@ -2,11 +2,15 @@
 
 function reducer(state, action) {
   console.log(state, action);
+  if (action.type === 'inc') return state + action.payload;
+  if (action.type === 'dec') return state - action.payload;
+  if (action.type === 'setCount') return action.payload;
 }
 function DateCounter() {
   // const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  // const [step, setStep] = useState(1);
 
+  const initialState = {count:0, step:1};
   const [count, dispatch] = useReducer(reducer, 0)
 
   // This mutates the date object.
@@ -16,16 +20,18 @@ function DateCounter() {
   const dec = function () {
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
+    dispatch({type:'dec', payload: 1})
   };
 
   const inc = function () {
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
-    dispatch(1)
+    dispatch({type: "inc", payload: 1})
   };
 
   const defineCount = function (e) {
     // setCount(Number(e.target.value));
+    dispatch({type:"setCount", payload: Number(e.target.value)})
   };
 
   const defineStep = function (e) {
